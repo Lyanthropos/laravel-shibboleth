17a435937cbff04a7b4634231c20310b77acea7b
Colin McFadden <cmcfadden@gmail.com>
2021-07-21 14:11:06 -0500

    cleanup route loading, support cases where register isn't defined

--- src/routes/shibalike.php
+++ src/routes/shibalike.php
@@ -1,9 +1,9 @@
 <?php
-if(config("shibboleth.register_routes")) {
+if(config("shibboleth.register_routes") !== false) { // use !== to handle case where it's not defined
     Route::group(['middleware' => 'web'], function () {
-        Route::get('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-        Route::post('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-        Route::get('emulated/login', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
-        Route::get('emulated/logout', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
+        Route::get('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+        Route::post('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+        Route::get('emulated/login', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
+        Route::get('emulated/logout', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
     });
 }
