bd3711398ce83644314d16d3439ead76ffcd1292
Colin McFadden <colin@divergentmedia.com>
2022-08-09 15:51:27 -0500

    Return arrays if shib gives us arrays
    trust the host app to deal with shib data properly.

--- src/StudentAffairsUwm/Shibboleth/Controllers/ShibbolethController.php
+++ src/StudentAffairsUwm/Shibboleth/Controllers/ShibbolethController.php
@@ -225,8 +225,7 @@ class ShibbolethController extends Controller
         if (config('shibboleth.sp_type') == "local_shib") {
             if(Request::session("shibAttributes")) {
                 $deserialized = unserialize(Request::session()->get("shibAttributes"));
-                return isset($deserialized[$variableName]) ?
-                (is_array($deserialized[$variableName])?$deserialized[$variableName][0]:$deserialized[$variableName]) : null;
+                return $deserialized[$variableName]?:null;
             }
         }
 
